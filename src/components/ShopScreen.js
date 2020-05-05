2a456ac4570bec4cf08e0619efc2ca765c0cb9f7
Killian Viquerat <kil.viquerat@gmail.com>
2020-05-06 00:03:22 +0200

    add View for shop

---
+++ src/components/ShopScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function ShopScreen() {
+  return (
+    <View>
+      <Text>Shop Screen</Text>
+    </View>
+  );
+}
+
+export default ShopScreen;

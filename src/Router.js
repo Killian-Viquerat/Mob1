0757da38c90b729efd00bce18958f7bad9658c59
Killian Viquerat <kil.viquerat@gmail.com>
2020-05-05 23:59:13 +0200

    Add tabNavigation for shop View

--- src/Router.js
+++ src/Router.js
@@ -2,21 +2,47 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/FontAwesome5';
 import HomeScreen from './components/HomeScreen.js';
 import SettingsScreen from './components/SettingsScreen.js';
 import LoginScreen from './components/LoginScreen.js';
+import RegisterScreen from './components/RegisterScreen.js';
+import ShopScreen from './components/ShopScreen.js';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 export default function Router() {
-  var logged = 0;
+  var logged = 1;
   if (logged) {
     return (
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: () => <Ionicons name="home" color="#333" size={24} />,
+            }}
+          />
+          <Tab.Screen
+            name="Shop"
+            component={ShopScreen}
+            options={{
+              tabBarIcon: () => (
+                <Ionicons name="shopping-basket" color="#333" size={24} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{
+              tabBarIcon: () => (
+                <Ionicons name="info-circle" color="#333" size={24} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     );
@@ -25,6 +51,7 @@ export default function Router() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
